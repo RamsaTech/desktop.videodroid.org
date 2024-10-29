1c8d4ae46376d34f4781b47e1c46d690eaecb506
RamsaTech <123633437+RamsaTech@users.noreply.github.com>
2024-10-29 07:56:30 +0530

    fix image

--- src/DesktopApp.tsx
+++ src/DesktopApp.tsx
@@ -185,7 +185,7 @@ function DesktopApp() {
                 <ul className="list-disc list-inside text-gray-600 space-y-2">
                   <li>This is the beta version, and the app is not signed yet. Your operating-system/browser might show a warning which can be safely ignored. You can see OS specific instructions here <a href='https://dl.videodroid.org/installation-guide' target='_blank'>https://dl.videodroid.org/installation-guide</a></li>
                   <li>On the first launch, allow the app to finish downloading <strong>ffmpeg, ffprobe, and the background video-grab engine</strong>. Attempting to download links before completion of this process will result in downloads not working. If the process gets stuck, try restarting the app.</li>
-                  <img src="/images/first-launch.png" alt="First Launch" className="my-4 rounded-lg shadow-md" />
+                  <img src="./images/first-launch.png" alt="First Launch" className="my-4 rounded-lg shadow-md" />
 
                   <li>This software is for personal use only, respect copyright laws and the terms of service of the websites you download from, We are not responsible for any misuse of this software.</li>
                   <li>For videos above 1080p, you might only see VP9 codec options. <Link to="/video-codecs" className="text-indigo-600 hover:underline" title="H.264 (AVC) is recommended for better compatibility with older devices and software like Quicktime player. While it's available for most videos up to 1080p, higher resolutions may only offer the modern higher quality VP9 codec.">Learn more about video codecs</Link></li>
