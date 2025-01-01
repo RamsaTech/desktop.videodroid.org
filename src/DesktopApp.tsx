ae8a0ae6df91a946d518c3d1dbd8b2636bd5edec
RamsaTech <123633437+RamsaTech@users.noreply.github.com>
2025-01-01 19:03:16 +0530

    Update DesktopApp.tsx v 7.1

--- src/DesktopApp.tsx
+++ src/DesktopApp.tsx
@@ -11,10 +11,10 @@ const osIcons = {
 };
 
 const downloadLinks = {
-  'Windows 7': 'https://www.yourvideofile.org/download/dist/full/windows/EasyVideoDownloaderExpress-0.6.0-beta.exe',
-  Windows: 'https://www.yourvideofile.org/download/dist/full/windows/EasyVideoDownloaderExpress-0.6.0-beta.exe',
-  macOS: 'https://www.yourvideofile.org/download/dist/full/mac/EasyVideoDownloaderExpress-0.6.0-beta-signed.pkg',
-  Linux: 'https://www.yourvideofile.org/download/dist/full/linux/EasyVideoDownloaderExpress-0.6.0-beta.AppImage',
+  'Windows 7': 'https://www.yourvideofile.org/download/dist/full/windows/EasyVideoDownloaderExpress-0.7.1-beta.exe',
+  Windows: 'https://www.yourvideofile.org/download/dist/full/windows/EasyVideoDownloaderExpress-0.7.1-beta.exe',
+  macOS: 'https://www.yourvideofile.org/download/dist/full/mac/EasyVideoDownloaderExpress-0.7.1-beta-signed.pkg',
+  Linux: 'https://www.yourvideofile.org/download/dist/full/linux/EasyVideoDownloaderExpress-0.7.1-beta.AppImage',
 };
 
 const features = [
@@ -183,7 +183,6 @@ function DesktopApp() {
               <div>
                 <h4 className="text-xl font-bold text-indigo-700 mb-4">Known Issues</h4>
                 <ul className="list-disc list-inside text-gray-600 space-y-2">
-                  <li>This is the beta version, and the app is not signed yet. Your operating-system/browser might show a warning which can be safely ignored. You can see OS specific instructions here <a href='https://dl.videodroid.org/installation-guide' target='_blank'>https://dl.videodroid.org/installation-guide</a></li>
                   <li>On the first launch, allow the app to finish downloading <strong>ffmpeg, ffprobe, and the background video-grab engine</strong>. Attempting to download links before completion of this process will result in downloads not working. If the process gets stuck, try restarting the app.</li>
                   <img src="./images/first-launch.png" alt="First Launch" className="my-4 rounded-lg shadow-md" />
                   <li>For videos above 1080p, you might only see VP9 codec options. <Link to="/video-codecs" className="text-indigo-600 hover:underline" title="H.264 (AVC) is recommended for better compatibility with older devices and software like Quicktime player. While it's available for most videos up to 1080p, higher resolutions may only offer the modern higher quality VP9 codec.">Learn more about video codecs</Link></li>
